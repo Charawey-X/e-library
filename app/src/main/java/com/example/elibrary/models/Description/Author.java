3762509b990ac4aa83fba204246b177aa6517410
Charawey-X <charawey@gmail.com>
2022-06-23 00:27:19 +0300

    add parceler to description models

--- app/src/main/java/com/example/elibrary/models/Description/Author.java
+++ app/src/main/java/com/example/elibrary/models/Description/Author.java
@@ -4,6 +4,10 @@ package com.example.elibrary.models.Description;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
+
+@Parcel
+
 public class Author {
 
     @SerializedName("author")
