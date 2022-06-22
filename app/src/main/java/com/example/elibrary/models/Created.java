8ceb4aebcc45bf834f6666c072a6cbbb81b439d7
Charawey-X <charawey@gmail.com>
2022-06-22 08:43:28 +0300

    add created model

---
+++ app/src/main/java/com/example/elibrary/models/Created.java
@@ -0,0 +1,52 @@
+
+package com.example.elibrary.models;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Created {
+
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("value")
+    @Expose
+    private String value;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Created() {
+    }
+
+    /**
+     * 
+     * @param type
+     * @param value
+     */
+    public Created(String type, String value) {
+        super();
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
