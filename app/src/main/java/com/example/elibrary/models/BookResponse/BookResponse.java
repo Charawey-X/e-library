ab8df91efbf8f1bdf198a85b286729f694b8fdb8
Charawey-X <charawey@gmail.com>
2022-06-22 17:30:42 +0300

    doc

---
+++ app/src/main/java/com/example/elibrary/models/BookResponse/BookResponse.java
@@ -0,0 +1,40 @@
+
+package com.example.elibrary.models.BookResponse;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class BookResponse {
+
+    @SerializedName("docs")
+    @Expose
+    private List<Doc> docs = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public BookResponse() {
+    }
+
+    /**
+     * 
+     * @param docs
+     */
+    public BookResponse(List<Doc> docs) {
+        super();
+        this.docs = docs;
+    }
+
+    public List<Doc> getDocs() {
+        return docs;
+    }
+
+    public void setDocs(List<Doc> docs) {
+        this.docs = docs;
+    }
+
+}
