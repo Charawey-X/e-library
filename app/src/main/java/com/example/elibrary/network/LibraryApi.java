b28b5282b34be42ac6d0cb7939bfbf2c72338d0a
Charawey-X <charawey@gmail.com>
2022-06-22 17:40:26 +0300

    change api get request

--- app/src/main/java/com/example/elibrary/network/LibraryApi.java
+++ app/src/main/java/com/example/elibrary/network/LibraryApi.java
@@ -1,4 +1,14 @@
 package com.example.elibrary.network;
 
+import com.example.elibrary.models.BookResponse.BookResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface LibraryApi {
+    @GET("/search.json")
+    Call<BookResponse> getResults(
+            @Query("q") String book
+    );
 }
