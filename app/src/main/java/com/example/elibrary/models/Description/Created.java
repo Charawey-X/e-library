04b9330045a61ce0b0a53b916906c8653b1ac9ff
Charawey-X <charawey@gmail.com>
2022-06-23 00:28:09 +0300

    add parceler to models

--- app/src/main/java/com/example/elibrary/models/Description/Created.java
+++ app/src/main/java/com/example/elibrary/models/Description/Created.java
@@ -4,7 +4,9 @@ package com.example.elibrary.models.Description;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
 
+@Parcel
 public class Created {
 
     @SerializedName("type")
