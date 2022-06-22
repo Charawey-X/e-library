ec2abc10bb8c2d99e5b8afd903155a968e356a69
Charawey-X <charawey@gmail.com>
2022-06-22 18:11:04 +0300

    edit main activity

--- app/src/main/java/com/example/elibrary/ui/MainActivity.java
+++ app/src/main/java/com/example/elibrary/ui/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.elibrary.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.elibrary.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.textView) EditText searchInput;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.button) Button submit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        submit.setOnClickListener(v -> {
+            String book = searchInput.getText().toString();
+            Intent intentSubmit = new Intent(MainActivity.this, WorksActivity.class);
+            intentSubmit.putExtra("book", book);
+            startActivity(intentSubmit);
+        });
     }
 }
--- app/src/main/java/com/example/elibrary/ui/WorksActivity.java
+++ app/src/main/java/com/example/elibrary/ui/WorksActivity.java
@@ -1,4 +1,16 @@
 package com.example.elibrary.ui;
 
-public class WorksActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.elibrary.R;
+
+public class WorksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_works);
+    }
 }
