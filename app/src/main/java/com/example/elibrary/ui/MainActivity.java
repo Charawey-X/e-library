94d81c47b99318c89388da2c626c6fdfabf0deea
Charawey-X <charawey@gmail.com>
2022-06-23 05:37:34 +0300

    add firebase

--- app/src/main/java/com/example/elibrary/ui/MainActivity.java
+++ app/src/main/java/com/example/elibrary/ui/MainActivity.java
@@ -2,15 +2,37 @@ package com.example.elibrary.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.elibrary.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.get_started_button)
+    Button mHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mHome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(MainActivity.this,R.string.welcome_toast,Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
